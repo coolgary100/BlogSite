ff57463e6e8f9a0305f43487d9179e7bc5cabf62
Bowen Jiang <bowengary@hotmail.com>
2023-06-15 18:43:00 -0700

    init, building up the post a bit

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img src="https://drive.google.com/uc?export=view&id=1se9ulzuJddNFAzYy3vO9_IMqUpDgEE6g" alt="drive image"/>
+        </div>
+        <div className="texts">
+          <h2>Face of surprise</h2>
+          <p>Wow look at the face of a beautiful baby who is surprised by what she drank</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://drive.google.com/uc?export=view&id=1se9ulzuJddNFAzYy3vO9_IMqUpDgEE6g" alt="drive image"/>
+        </div>
+        <div className="texts">
+          <h2>Face of surprise</h2>
+          <p>Wow look at the face of a beautiful baby who is surprised by what she drank</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://drive.google.com/uc?export=view&id=1se9ulzuJddNFAzYy3vO9_IMqUpDgEE6g" alt="drive image"/>
+        </div>
+        <div className="texts">
+          <h2>Face of surprise</h2>
+          <p>Wow look at the face of a beautiful baby who is surprised by what she drank</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
